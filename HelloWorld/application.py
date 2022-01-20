948b5a99ba5a83ed97b3dfb534f88b842fcb78ab
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-01-19 18:03:20 -0700

    feat: Completed 2nd and 3rd tutorial.

--- HelloWorld/application.py
+++ HelloWorld/application.py
@@ -1,1 +1,22 @@
-print("Hello world!")
+# print("Hello world!")
+# print("This is the next statement.")
+#
+# first_name = "T'pol "
+# last_name = "Vulcan"
+#
+# name = first_name + last_name
+#
+# right = "right"
+#
+# sentence = first_name + last_name + " has a perfect " + str(10) + " body" + ", " + right + "!!!"
+#
+# print(sentence)
+
+# sexy = True
+#
+# data_type = type("Seven of Nine")
+#
+# data_type = type(sexy)
+#
+# print(data_type)
+
