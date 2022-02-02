54fa4db2d6360f14b146473c86e0989e21c3aa93
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-02-02 05:55:22 -0700

    chore: Completed basic arithmetic operations.

--- HelloWorld/application.py
+++ HelloWorld/application.py
@@ -20,3 +20,16 @@
 #
 # print(data_type)
 
+num1 = 3
+
+num2 = 10
+
+# Modulus to get remainder
+modulus_answer = num2 % num1
+
+print(modulus_answer)
+
+# Order of operations (O_o_O)
+O_o_O_answer = (10 + 3) * (9 - 4)
+
+print(O_o_O_answer)
